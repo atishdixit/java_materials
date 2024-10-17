5087b08ec47fd241add7c3fad326ce787a54019a
atishdixit <atishdixit26@gmail.com>
2024-10-17 09:23:51 +0530

    Demo

---
+++ lecture_01/demo/src/com/ext/lect/Starter.java
@@ -0,0 +1,29 @@
+package com.ext.lect;
+
+import com.ext.lect.model.User;
+
+public class Starter {
+
+    public static void main(String[] args) {
+       User u1 =  new User();
+        User u2 =  new User();
+        u2.wrongHit();
+        User u3 =  new User();
+        User u4 =  new User();
+        u4.wrongHit();
+        User u5 =  new User();
+        User u6 =  new User();
+        u6.wrongHit();
+//        System.out.println("Total number of object on heap "+User.getCount());
+
+
+        System.out.println("Objects: "+User.getCount());
+        System.out.println(User.audit());
+
+        //out of Hundrad Object, if 06 worng, system on risk, 50-> Alert, lest 50 ok
+
+
+
+
+    }
+}
